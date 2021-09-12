6f944c0e9b319c940bc5fe581ccf9e031766a317
dillon walsh <dillonwalsh93@gmail.com>
2021-09-12 13:08:11 -0400

    added apollo server and middleware

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+server.applyMiddleWare({app})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -17,5 +28,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`useGraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
